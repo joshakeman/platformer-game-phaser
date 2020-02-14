6ed52400bf990f0d03898c40f63c986160a449cb
Josh Akeman <joshakeman@gmail.com>
2020-02-14 13:03:57 -0500

    sprite moving

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/phaser.d.ts" />
 import Phaser from 'phaser';
 
 class Game extends Phaser.Scene {
@@ -8,11 +9,23 @@ class Game extends Phaser.Scene {
   init(data) {}
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+
+    this.anims.create({
+      key: 'hero-running',
+      frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.player = this.add.sprite(400, 300, 'hero-run-sheet');
+    this.player.anims.play('hero-running');
   }
 
   update(time, delta) {}
